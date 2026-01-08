701e335e85b6c2388a7de4be1a21b3acc956415c
casa1410 <sacasadiego@gmail.com>
2026-01-07 23:11:49 -0500

    Add MSW handlers and server setup for mocking API responses in tests

---
+++ src/tests/setup.ts
@@ -0,0 +1,12 @@
+import "@testing-library/jest-dom";
+
+import { afterAll, afterEach, beforeAll } from "vitest";
+import { cleanup } from "@testing-library/react";
+import { server } from "@/msw/server";
+
+beforeAll(() => server.listen());
+afterEach(() => {
+  server.resetHandlers();
+  cleanup();
+});
+afterAll(() => server.close());
